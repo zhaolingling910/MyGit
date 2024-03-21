f6833d69df6b97e1c4f0d7676dcc048a888d37a1
赵玲玲 <zhaolingling_910@163.com>
2024-03-21 17:28:26 +0800

    第一次提交

--- app/src/main/java/com/example/mygit/MainActivity.kt
+++ app/src/main/java/com/example/mygit/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mygit.ui.theme.MyGitTheme
 
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
